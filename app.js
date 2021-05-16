48e3fe2c4b4ec9cda55c9c31b078edfffad092f1
Vedran Sikic <sikic.vedran@gmail.com>
2021-05-16 16:54:42 +0200

    AWS deployment

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ const stockRouter = require('./src/routers/stock-router')
 require('./src/db/mongoose')
 
 const app = express();
-const port = 4000;
+const port = 8080||process.env.JWT_SECRET;
 
 app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(stockRouter)
+
+app.get('/', (req, res) => {
+    res.send('Welcome!')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
